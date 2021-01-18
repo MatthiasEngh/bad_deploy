fbdf048a34c1920d7312731c02a41f0f4774f4ab
Matthias Engh <matthias.engh@gmail.com>
2021-01-17 19:45:03 -0600

    require name and path for clarity

--- deploy.zsh
+++ deploy.zsh
@@ -11,7 +11,7 @@ app_scp_config="${app_dir}/scp.conf"
 echo "prepared to read scp config: ${app_scp_config}"
 source $app_scp_config
 
-scp_target=${ssh_host}:${scp_target_path}
+scp_target=${ssh_host}:${scp_target_path}/${scp_target_name}
 full_scp_source_path=$HOME/${scp_source_dir}
 echo "prepared to copy ${full_scp_source_path} to ${scp_target} as ${ssh_user}"
 scp -i ${ssh_identity_file} -r ${full_scp_source_path} ${ssh_user}@$scp_target
